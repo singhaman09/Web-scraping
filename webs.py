305693b827be8121b8dbc891d15657e209f47229
Aman Singh Rathore <112258507+singhaman09@users.noreply.github.com>
2024-07-14 21:46:33 +0530

    Update webs.py

--- webs.py
+++ webs.py
@@ -7,39 +7,39 @@ url="https://www.simplilearn.com/"
 r= requests.get(url)
 htmlContent= r.content
 
-# print(htmlContent)
+print(htmlContent)
 #BeautifulSoup used to extract html and xml file i.e extract data.
 
 soup= BeautifulSoup(r.content,'html.parser')
-# for i in soup.find_all("code"):
-#     print(i.text)
+for i in soup.find_all("code"):
+    print(i.text)
 
-# title = soup.title
-# print(title.string)
+title = soup.title
+print(title.string)
 
-# a=soup.find_all('p')
-# for i in a:
-#     print(i)
+a=soup.find_all('p')
+for i in a:
+    print(i)
 
-# print(soup.find('p')[6]['class'])
-# second_p = soup.find_all('p')[7]  # Find all <p> tags and get the seventh one
-# print(second_p['class']) #find class of second p
+print(soup.find('p')[6]['class'])
+second_p = soup.find_all('p')[7]  # Find all <p> tags and get the seventh one
+print(second_p['class']) #find class of second p
 
-# print(soup.find('p').get_text())
+print(soup.find('p').get_text())
 
 anchor=soup.find_all('a')
-# print(anchor)
+print(anchor)
 
-# for i in anchor:
-#     print(i.get('href'))
+for i in anchor:
+    print(i.get('href'))
 
 al = set()
-# for i in anchor:
-#     href = i.get('href')
-#     if href is not None:
-#         text = "https://www.simplilearn.com/" + href
-#         al.add(text)
-#         print(text)
+for i in anchor:
+    href = i.get('href')
+    if href is not None:
+        text = "https://www.simplilearn.com/" + href
+        al.add(text)
+        print(text)
 
 root=soup.find(id='root')
 print(root)
